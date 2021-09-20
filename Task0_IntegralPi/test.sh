687792ca5caf94f37a7f2c02c1d95d3ad7daf386
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2021-09-20 12:01:53 +0300

    Added shell script test.sh

---
+++ Task0_IntegralPi/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Test 1 --- 10 1"
+./main 10 1
+echo "Test 2 --- 100 15"
+./main 100 15
+echo "Test 3 --- 500 12"
+./main 500 12
+echo "Test 4 --- 250 6"
+./main 250 6
+echo "Test 5 --- 100000 100"
+./main 100000 100
