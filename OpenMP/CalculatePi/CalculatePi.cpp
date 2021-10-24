8ed6fd48ddd9fe4fe78dc2c3115f8c8d74e53754
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2021-10-24 15:19:31 +0300

    Update OpenMP/CalculatePi

--- OpenMP/CalculatePi/CalculatePi.cpp
+++ OpenMP/CalculatePi/CalculatePi.cpp
@@ -9,7 +9,9 @@ int main() {
     double h = 1.0 / N;
     double sum = 0.0;
     double x, pi;
+    double start, end;
 
+    start = omp_get_wtime();
 #pragma omp parallel default(none) shared(h, sum) private(x) num_threads(1000)
     {
 #pragma omp for reduction(+:sum) schedule(static, N/omp_get_num_threads())
@@ -18,8 +20,10 @@ int main() {
             sum += 4.0 / (1.0 + x * x);
         }
     }
+    end = omp_get_wtime();
 
     pi = h * sum;
     std::cout << std::setprecision(30);
-    std::cout << "Calculated Pi: " << pi << "\n";
+    std::cout << "Calculated Pi: " << pi << ".\n";
+    std::cout << std::setprecision(5) << "Elapsed Time: " << end-start << "\n";
 }
