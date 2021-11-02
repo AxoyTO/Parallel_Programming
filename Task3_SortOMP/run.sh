584a35fda8e96575ede0d5bdd5f0922cd747729c
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2021-11-02 23:23:11 +0300

    (+) Makefile and run.sh script

--- Task3_SortOMP/run.sh
+++ Task3_SortOMP/run.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
+echo "-------------  RUN #1 -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 1"
+./SortOMP 1000000 1
+echo ""
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 12"
-./SortOMP 1000000 12 
+echo "-------------  RUN #2  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 2"
+./SortOMP 1000000 2
+echo ""
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 24"
-./SortOMP 1000000 24
+echo "-------------  RUN #3  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 4"
+./SortOMP 1000000 4
+echo ""
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 48"
-./SortOMP 1000000 48
+echo "-------------  RUN #4  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 8"
+./SortOMP 1000000 8
+echo ""
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 72"
-./SortOMP 1000000 72
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 96"
-./SortOMP 1000000 96
+echo "-------------  RUN #5  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 16"
+./SortOMP 1000000 16
+echo ""
 
-echo "\n *  ARRAY SIZE: 1000000\n *  THREADS: 128"
-./SortOMP 1000000 128
+echo "-------------  RUN #6  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 32"
+./SortOMP 1000000 32
+echo ""
 
+
+echo "-------------  RUN #7  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 64"
+./SortOMP 1000000 64
+echo ""
+
+# in POLUS(POWR8) -> 10 cores in each socket. 8 threads per core. -> 80 threads per socket
+echo "-------------  RUN #8  -------------"
+echo " *  ARRAY SIZE: 1000000\n *  THREADS: 80"
+./SortOMP 1000000 80
+echo ""
+
+: 'ARRAY SIZE: 2000000 -> SEGMENTATION FAULT IN POLUS...
 echo "\n *  ARRAY SIZE: 2000000\n *  THREADS: 12"
 ./SortOMP 2000000 12
 
@@ -36,4 +61,4 @@ echo "\n *  ARRAY SIZE: 2000000\n *  THREADS: 96"
 
 echo "\n *  ARRAY SIZE: 2000000\n *  THREADS: 128"
 ./SortOMP 2000000 128
-
+'
