2fe8c46952d886094b170ba17798fddc7ce3374f
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2022-05-15 18:29:41 +0300

    Added comments for calculating parallel exectime

--- 2_Householder_MPI/tests.cpp
+++ 2_Householder_MPI/tests.cpp
@@ -17,11 +17,15 @@ int check_matrix_singularity(const Matrix& A,
                              int rank,
                              int comm_size) {
   int error_flag = 0;
-  for (int i = 0; i < A.size(); i++)
+  for (int i = 0; i < A.size(); i++) {
+    // std::cout << "rank:" << A[i].rank << "   " <<
+    // std::abs(A[i].col[A[i].rank])
+    //       << "\n";
     if (std::abs(A[i].col[A[i].rank]) < 10e-7) {
       error_flag = 1;
       break;
     }
+  }
 
   int tmp;
   MPI_Allreduce(&error_flag, &tmp, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
